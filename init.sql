f754e53cfd752aba2e239be92c8c14cf7ac0b2ac
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-04-21 19:40:57 +0530

    DATABASE INITIALIZATION Create a file for initializing the table with an entry.

--- init.sql
+++ init.sql
@@ -1,16 +1,18 @@
 CREATE TABLE lists(
-    list_id bigserial PRIMARY KEY NOT NULL,
+    list_id SERIAL PRIMARY KEY NOT NULL,
     list_name VARCHAR(255) NOT NULL
 )
 
-CREATE TABLE tasks(
-    task_id bigserial PRIMARY KEY NOT NULL,
-    task_name VARCHAR(255) NOT NULL,
-    scheduled VARCHAR(255),
-    completed boolean DEFAULT FALSE,
-    priority INTEGER DEFAULT 0,
-    note text,
-    list_id bigserial
-)
+INSERT INTO lists (list_name) VALUES ('Shopping of book')
 
-ALTER TABLE tasks ADD CONSTRAINT fk_lists_tasks FOREIGN KEY (list_id) REFERENCES lists (list_id) ON DELETE CASCADE
+-- CREATE TABLE tasks(
+--     task_id SERIAL PRIMARY KEY NOT NULL,
+--     task_name VARCHAR(255) NOT NULL,
+--     scheduled VARCHAR(255),
+--     completed boolean DEFAULT FALSE,
+--     priority INTEGER DEFAULT 0,
+--     note text,
+--     list_id INTEGER
+-- )
+
+-- ALTER TABLE tasks ADD CONSTRAINT fk_lists_tasks FOREIGN KEY (list_id) REFERENCES lists (list_id) ON DELETE CASCADE
