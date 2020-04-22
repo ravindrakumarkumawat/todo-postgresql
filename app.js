6aa90585838e9d60e91a1f0d50568d0b4b270777
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-04-22 18:38:55 +0530

    Add post route for creating list

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+const pool  = require('./config')
 
 const listsRouter = require('./routes/lists')
 
 const app = express()
 
 // view engine setup
-app.use(express.json());
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+app.use(cors())
+app.use(express.json()) // req.body
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', listsRouter)
-app.use('/lists', listsRouter)
+// ROUTES
+
+// Create a list
+app.post('/lists', async (req, res) => {
+    try {
+        const { list_name }  = req.body
+        console.log(list_name)
+        const newList = await pool.query("INSERT INTO lists (list_name) VALUES ($1)", [list_name])
+        res.json(newList)
+    } catch (error) {
+        console.log(error.message)
+   }
+})
+
+// get all lists
+
+// get a list
+
+// Update a list
+
+// Delete a list
+
+//app.use('/lists', listsRouter)
 
 
 // Start server
-app.listen(process.env.PORT || 3002, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Server listening`)
 })
