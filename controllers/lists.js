ef9e27906cda20f9e05908405a5a6dce34273818
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-04-23 00:09:28 +0530

    Seperate the controllers and routes for the lists

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,26 +1,53 @@
-// const { pool } = require('../config')
+const { pool } = require('../config')
 
-// exports.get_lists = async(req, res) => {
-//     pool.query('SELECT * FROM lists', (error, results) => {
-//         if (error) throw error
-//         res.send(results.rows)
-//     })
-// }
+exports.get_lists = async (req, res) => {
+    try {
+        const allLists = await pool.query("SELECT * FROM lists")
+        res.json(allLists.rows)
+    } catch (err) {
+        console.log(err.message)
+   }
+}
 
-// exports.get_list = (req, res, next) => {
-//     const listId = req.params.id
-//     pool.query('SELECT * FROM lists WHERE list_id = $1', [listId], (error, results) => {
-//         if (error) throw error
-//         res.send(results.rows[0].list_name)
-//     })
-// }
+exports.add_list = async (req, res) => {
+    try {
+        const { list_name }  = req.body
+        const newList = await pool.query("INSERT INTO lists (list_name) VALUES ($1) RETURNING *", [list_name])
+        res.json(newList.rows[0])
+    } catch (err) {
+        console.log(err.message)
+   }
+}
 
-// exports.add_list = async (req, res, next) => {
-//     try {
-//         const { list_name } = req.body
-//         const newList = await pool.query("INSERT INTO lists (list_name) VALUES $1", [list_name])
-//         res.json(newList)
-//     } catch (error) {
-        
-//     }
-// }
+
+exports.get_list = async (req, res) => {
+    try {
+        //console.log(req.params)
+        const { id } = req.params
+        const list = await pool.query("SELECT * FROM lists WHERE list_id = $1", [id])
+        res.json(list.rows[0])
+    } catch (err) {
+        console.log(err.message)
+   }
+}
+
+exports.update_list = async (req, res) => {
+    try {
+        const {id} = req.params
+        const { list_name }  = req.body
+        const updateList = await pool.query("UPDATE lists SET list_name = $1 WHERE list_id = $2", [list_name, id])
+        res.json("List name is updated")
+    } catch (err) {
+        console.log(err.message)
+   }
+}
+
+exports.delete_list = async (req, res) => {
+    try {
+        const {id} = req.params
+        const deleteList = await pool.query("DELETE FROM lists WHERE list_id = $1", [id])
+        res.json("List is deleted")
+    } catch (err) {
+        console.log(err.message)
+   }
+}
