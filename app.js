1b4c5bd1c06bf15c2912c004f8e6aaffcc9a422b
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-04-23 00:07:59 +0530

    Seperate the route

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const pool  = require('./config')
 
 const listsRouter = require('./routes/lists')
 
@@ -16,65 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ROUTES
-
-// Create a list
-app.post('/lists', async (req, res) => {
-    try {
-        const { list_name }  = req.body
-        const newList = await pool.query("INSERT INTO lists (list_name) VALUES ($1) RETURNING *", [list_name])
-        res.json(newList.rows[0])
-    } catch (err) {
-        console.log(err.message)
-   }
-})
-
-// get all lists
-app.get('/lists', async (req, res) => {
-    try {
-        const allLists = await pool.query("SELECT * FROM lists")
-        res.json(allLists.rows)
-    } catch (err) {
-        console.log(err.message)
-   }
-})
-
-// get a list
-app.get('/lists/:id', async (req, res) => {
-    try {
-        //console.log(req.params)
-        const { id } = req.params
-        const list = await pool.query("SELECT * FROM lists WHERE list_id = $1", [id])
-        res.json(list.rows[0])
-    } catch (err) {
-        console.log(err.message)
-   }
-})
-
-// Update a list
-app.put('/lists/:id', async (req, res) => {
-    try {
-        const {id} = req.params
-        const { list_name }  = req.body
-        const updateList = await pool.query("UPDATE lists SET list_name = $1 WHERE list_id = $2", [list_name, id])
-        res.json("List name is updated")
-    } catch (err) {
-        console.log(err.message)
-   }
-})
-// Delete a list
-
-app.delete('/lists/:id', async (req, res) => {
-    try {
-        const {id} = req.params
-        const deleteList = await pool.query("DELETE FROM lists WHERE list_id = $1", [id])
-        res.json("List is deleted")
-    } catch (err) {
-        console.log(err.message)
-   }
-})
-
-//app.use('/lists', listsRouter)
-
+app.use('/lists', listsRouter)
 
 // Start server
 app.listen(process.env.PORT || 5000, () => {
