af9eaade575a0f0d41328b57cd3057b2a9130b47
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-04-22 18:40:43 +0530

    Comment all the data that are present inside those file for now

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,20 +1,26 @@
-const { pool } = require('../config')
+// const { pool } = require('../config')
 
-exports.get_lists = (req, res, next) => {
-    pool.query('SELECT * FROM lists', (error, results) => {
-        if (error) throw error
-        res.send(results.rows)
-    })
-}
+// exports.get_lists = async(req, res) => {
+//     pool.query('SELECT * FROM lists', (error, results) => {
+//         if (error) throw error
+//         res.send(results.rows)
+//     })
+// }
 
-exports.get_list = (req, res, next) => {
-    const listId = req.params.id
-    pool.query('SELECT * FROM lists WHERE list_id = $1', [listId], (error, results) => {
-        if (error) throw error
-        res.send(results.rows[0].list_name)
-    })
-}
+// exports.get_list = (req, res, next) => {
+//     const listId = req.params.id
+//     pool.query('SELECT * FROM lists WHERE list_id = $1', [listId], (error, results) => {
+//         if (error) throw error
+//         res.send(results.rows[0].list_name)
+//     })
+// }
 
-exports.add_list = (req, res, next) => {
-    res.send('added list')
-}
+// exports.add_list = async (req, res, next) => {
+//     try {
+//         const { list_name } = req.body
+//         const newList = await pool.query("INSERT INTO lists (list_name) VALUES $1", [list_name])
+//         res.json(newList)
+//     } catch (error) {
+        
+//     }
+// }
--- routes/lists.js
+++ routes/lists.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const router = express.Router()
+// const express = require('express')
+// const router = express.Router()
 
-const lists =  require('../controllers/lists')
+// const lists =  require('../controllers/lists')
 
-router.get('/', lists.get_lists)
-router.get('/:id', lists.get_list)
-// router.post('/lists', lists.add_list)
-// router.put('/:id', lists.update_list)
-// router.delete('/:id', lists.delete_list)
+// router.get('/', lists.get_lists)
+// router.get('/:id', lists.get_list)
+// router.post('/', lists.add_list)
+// // router.put('/:id', lists.update_list)
+// // router.delete('/:id', lists.delete_list)
 
-module.exports = router
+// module.exports = router
