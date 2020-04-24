a95f497b9db431d98d55f90f85a270487075dd2d
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-04-24 19:12:34 +0530

    body parser added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const listsRouter = require('./routes/lists')
@@ -10,6 +11,7 @@ const app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser())
 app.use(cors())
 app.use(express.json()) // req.body
 app.use(express.urlencoded({ extended: false }));
